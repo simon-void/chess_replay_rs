61969f4f57b751357f84ff681de93b57531388eb
stephans <simon.void@gmx.de>
2024-07-22 19:47:24 +0200

    logs decompression result, but no chess board animation yet

--- docs/src/decode_chess_wasm/decode_chess_wasm.js
+++ docs/src/decode_chess_wasm/decode_chess_wasm.js
@@ -213,7 +213,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('chess_logic_wasm_bg.wasm', import.meta.url);
+        input = new URL('decode_chess_wasm_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
