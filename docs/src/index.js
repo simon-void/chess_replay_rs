4d648e3ed11169af47be2897ab122168225384e2
stephans <simon.void@gmx.de>
2024-07-24 06:46:22 +0200

    compressed match can now be provided via query param named 'moves'

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,11 +1,46 @@
 import init, * as wasm from './decode_chess_wasm/decode_chess_wasm.js';
-import {Chessboard, INPUT_EVENT_TYPE, MOVE_INPUT_MODE} from "./cm-chessboard/Chessboard.js"
+import {Chessboard, MOVE_INPUT_MODE} from "./cm-chessboard/Chessboard.js"
 
 
 let init_Promise = init();
 
 async function init_wasm() {
     await init_Promise;
+}
+
+window.onload = function () {
+    // The site displays a warning if WebAssembly isn't available in the user's browser.
+    // But if this point is reached, WebAssembly is present and the warning can be disabled and the main content enabled.
+    let noWasmWarning = document.getElementById("no-wasm-warning");
+    let pageLoadingSpinner = document.getElementById("page-loading");
+    let mainDiv = document.getElementById("main");
+
+    const queryParams = new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => searchParams.get(prop),
+    });
+    // "Y3vghpnyfWW7Q" -> "a2a4, h7h6, a4a5, b7b5, a5b6, h6h5, b6c7, h5h4, g2g3, h4g3, c7d8Q"
+    // "TuCU2BS-tDL8_EA" -> "d2d3, g7g6, c1e3, f8g7, b1c3, g8f6, d1d2, e8h8, e1a1"
+    const defaultMatch = "TuCU2BS-tDL8_EA";
+    const queryMatch = queryParams.moves;
+    const effectiveMatch = queryMatch ? queryMatch : defaultMatch;
+
+    if(effectiveMatch===defaultMatch) {
+        log("no query parameter 'moves' found. using default match: "+defaultMatch);
+    }
+
+    init_wasm().then(_ => {
+        decompress(effectiveMatch).then(gameData => {
+            pageLoadingSpinner.remove();
+            mainDiv.style = "display: block";
+
+            let gameModel = new GameState(gameData);
+            ko.applyBindings(gameModel);
+        });
+    }, reason => {
+        pageLoadingSpinner.remove();
+        noWasmWarning.style = "display: block; background-color: lightpink";
+        console.log("init_wasm failed with: "+reason);
+    });
 }
 
 async function decompress(urlsafe_game_notation) {
@@ -25,11 +60,10 @@ async function decompress(urlsafe_game_notation) {
 }
 
 function GameData() {
-    let self = {
+    return {
         moves: [],
         positions: ["rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"],  // classic starting position
     };
-    return self;
 }
 
 const output = document.getElementById("output")
@@ -38,34 +72,6 @@ function log(text) {
     const log = document.createElement("div");
     log.innerText = text;
     output.prepend(log);
-}
-
-window.onload = function () {
-    // The site displays a warning if WebAssembly isn't available in the user's browser.
-    // But if this point is reached, WebAssembly is present and the warning can be disabled and the main content enabled.
-    let noWasmWarning = document.getElementById("no-wasm-warning");
-    let pageLoadingSpinner = document.getElementById("page-loading");
-    let mainDiv = document.getElementById("main");
-
-    init_wasm().then(_ => {
-        // gameModel.state(states.HUMAN_TURN);
-        // "Y3vghpnyfWW7Q" -> "a2a4, h7h6, a4a5, b7b5, a5b6, h6h5, b6c7, h5h4, g2g3, h4g3, c7d8Q"
-        // "TuCU2BS-tDL8_EA" -> "d2d3, g7g6, c1e3, f8g7, b1c3, g8f6, d1d2, e8h8, e1a1"
-        let compressed_game = "TuCU2BS-tDL8_EA";
-        decompress(compressed_game).then(gameData => {
-            pageLoadingSpinner.remove();
-            mainDiv.style = "display: block";
-
-            let gameModel = new GameState(gameData);
-            ko.applyBindings(gameModel);
-        });
-
-        log("match in compressed notation: "+compressed_game);
-    }, reason => {
-        pageLoadingSpinner.remove();
-        noWasmWarning.style = "display: block; background-color: lightpink";
-        console.log("init_wasm failed with: "+reason);
-    });
 }
 
 function UiModel(gameState) {
@@ -88,7 +94,7 @@ function UiModel(gameState) {
         // log("new fen: "+ fen)
         self.board.setPosition(fen);
     }
-    self.setProgress = function (fen) {
+    self.setProgress = function () {
         let progressDiv = document.getElementById("progress");
         return function (msg) {
             progressDiv.innerText = msg;
